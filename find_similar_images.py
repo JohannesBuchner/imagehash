422ac5437c5112d1f277419c00c60b8693f61ca7
EnricoMiccoli <EnricoMiccoli@users.noreply.github.com>
2016-09-07 12:36:41 +0200

    Fixes image finder demo Replaced iteritems with items, removed tabs indentation

--- find_similar_images.py
+++ find_similar_images.py
@@ -1,32 +1,28 @@
 #!/usr/bin/env python
 from PIL import Image
 import imagehash
+import os
+import sys
 
-"""
-Demo of hashing
-"""
+IMAGE_EXTENSIONS = {".bmp", ".gif", ".jpeg", ".jpg", ".png"}
+
 def find_similar_images(userpath, hashfunc = imagehash.average_hash):
-    import os
     def is_image(filename):
-    	f = filename.lower()
-    	return f.endswith(".png") or f.endswith(".jpg") or \
-    		f.endswith(".jpeg") or f.endswith(".bmp") or f.endswith(".gif")
+        return os.path.splitext(filename)[1].lower() in IMAGE_EXTENSIONS
     
     image_filenames = [os.path.join(userpath, path) for path in os.listdir(userpath) if is_image(path)]
     images = {}
     for img in sorted(image_filenames):
-    	hash = hashfunc(Image.open(img))
-    	images[hash] = images.get(hash, []) + [img]
+        hash = hashfunc(Image.open(img))
+        images[hash] = images.get(hash, []) + [img]
     
-    for k, img_list in images.iteritems():
-    	if len(img_list) > 1:
-    		print(" ".join(img_list))
+    for k, img_list in images.items():
+        if len(img_list) > 1:
+            print(" ".join(img_list))
 
-
-if __name__ == '__main__':
-    import sys, os
-    def usage():
-    	sys.stderr.write("""SYNOPSIS: %s [ahash|phash|dhash|...] [<directory>]
+def usage():
+    sys.stderr.write(
+"""SYNOPSIS: %s [ahash|phash|dhash|...] [<directory>]
 
 Identifies similar images in the directory.
 
@@ -38,23 +34,27 @@ Method:
   whash-db4:  Daubechies wavelet hash
 
 (C) Johannes Buchner, 2013
-""" % sys.argv[0])
-    	sys.exit(1)
-    
-    hashmethod = sys.argv[1] if len(sys.argv) > 1 else usage()
-    if hashmethod == 'ahash':
-    	hashfunc = imagehash.average_hash
-    elif hashmethod == 'phash':
-    	hashfunc = imagehash.phash
-    elif hashmethod == 'dhash':
-    	hashfunc = imagehash.dhash
-    elif hashmethod == 'whash-haar':
-    	hashfunc = imagehash.whash
-    elif hashmethod == 'whash-db4':
-    	hashfunc = lambda img: imagehash.whash(img, mode='db4')
-    else:
-    	usage()
-    userpath = sys.argv[2] if len(sys.argv) > 2 else "."
-    find_similar_images(userpath=userpath, hashfunc=hashfunc)
-    
+""" % sys.argv[0]
+    )
+    sys.exit(1)
 
+if __name__ == '__main__':
+    argc = len(sys.argv)
+    if argc == 2:
+        userpath = "."
+    elif argc == 3:
+        userpath = sys.argv[2]
+    else:
+        usage()
+    hashmethod = sys.argv[1]
+    try:
+        hashfunc = {
+            'ahash': imagehash.average_hash,
+            'phash': imagehash.phash,
+            'dhash': imagehash.dhash,
+            'whash-haar': imagehash.whash,
+            'whash-db4': lambda img: imagehash.whash(img, mode='db4'),
+        }[hashmethod]
+    except KeyError:
+        usage()
+    find_similar_images(userpath=userpath, hashfunc=hashfunc)

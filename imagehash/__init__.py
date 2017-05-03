99d25a5ec4814d0e5f7336f54da9d506f7a922fe
Evgeny <mastergenius@yandex.ru>
2017-05-03 15:42:31 +0300

    Do not read version from file for pyinstaller compatibility

--- imagehash/__init__.py
+++ imagehash/__init__.py
@@ -36,8 +36,8 @@ import numpy
 #import scipy.fftpack
 #import pywt
 import os.path
-__version__ = open(os.path.join(os.path.abspath(
-	os.path.dirname(__file__)), 'VERSION')).read().strip()
+#__version__ = open(os.path.join(os.path.abspath(
+#	os.path.dirname(__file__)), 'VERSION')).read().strip()
 
 def _binary_array_to_hex(arr):
 	"""

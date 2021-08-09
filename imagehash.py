7ce2dc30dede1437919445e691bbb250add56345
Alexander Piskun <bigcat88@icloud.com>
2021-08-09 14:38:33 +0300

    fast _binary_array_to_hex
    lets numpy do it for us.

--- imagehash.py
+++ imagehash.py
@@ -76,9 +76,7 @@ def _binary_array_to_hex(arr):
 	"""
 	internal function to make a hex string out of a binary array.
 	"""
-	bit_string = ''.join(str(b) for b in 1 * arr.flatten())
-	width = int(numpy.ceil(len(bit_string)/4))
-	return '{:0>{width}x}'.format(int(bit_string, 2), width=width)
+	return numpy.packbits(arr, axis=None).tobytes().hex()
 
 
 class ImageHash(object):
